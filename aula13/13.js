dd12286437f2b864389e639b41c7e502a28ea593
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-06-28 11:04:51 -0300

    Create 13.js

---
+++ aula13/13.js
@@ -0,0 +1,7 @@
+let num=240
+
+if (num > 150 && num <=200){
+    console.log("o numeral " + num + " é maior")
+} else if (num >= 200 && num <= 250){
+    console.log("o numeral " + num + " está entre 200 e 250")
+}
