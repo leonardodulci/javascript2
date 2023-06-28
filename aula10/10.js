8fe1e339b1976d46be8eeb1c6ad44d02e7072012
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-06-27 23:14:31 -0300

    js

--- aula10/10.js
+++ aula10/10.js
@@ -1,3 +1,5 @@
 let num = 11
 let res =(!(num%2) ? "par" : "impar")
 console.log(res)
+
+//operador ternario
---
+++ aula12/12.js
@@ -0,0 +1,16 @@
+/*
+let n1 = [10, 20, 30]
+let n2 = [11, 22, 33, 44, 55]
+let n3 = [...n1, ...n2]
+
+console.log("n1: " + n1)
+console.log("n2: " + n2)
+console.log("n3: " + n3)
+*/
+
+const obj1 = document.getElementsByName("div")
+const obj2 = [...document.getElementsByTagName("div")]
+
+//obj2.
+
+//conseguimos mais opções com spread
