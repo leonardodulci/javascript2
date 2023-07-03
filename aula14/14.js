e643396e90cdb95ff9406b143cd0bb946e8098df
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-07-03 15:14:08 -0300

    .

---
+++ aula14/14.js
@@ -0,0 +1,20 @@
+//switch case
+
+let colocacao = 17
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de consolação")
+        break
+    default:
+        console.log("Não subiu ao podio")
+}
